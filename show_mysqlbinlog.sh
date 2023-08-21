f44cc9d9e0e5413b78df729a61915e3576428b82
okue <yuya.okuyama@linecorp.com>
2023-08-21 13:36:33 +0900

    Use `mysqlbinlog` command in `mysql:8-debian` image

--- show_mysqlbinlog.sh
+++ show_mysqlbinlog.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-LOG_FILE_NAME=`cd mysql/data/; ls -1t mysql-bin.* | head -n 1`
+LOG_FILE_NAME=`cd mysql/data/; ls -1t mysql-bin.0* | head -n 1`
 
 echo mysqlbinlog --host=localhost --port=3306 --protocol=TCP -R --user=root --stop-never -vv $LOG_FILE_NAME
-mysqlbinlog --host=localhost --port=3306 --protocol=TCP -R --user=root --stop-never -vv $LOG_FILE_NAME
+docker exec -it mysql mysqlbinlog --host=localhost --port=3306 --protocol=TCP -R --user=root --stop-never -vv $LOG_FILE_NAME
